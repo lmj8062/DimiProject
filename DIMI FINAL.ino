457cfb91573e4e952a3562f133d2f3416e8d79d8
Leeminjae <lmj8062@naver.com>
2017-10-21 23:45:31 +0900

    DIMIGO PROJECT FINAL
    MFRC552.H 소스파일 일부, 헤더파일 제공 :
    http://github.com/miguelbalboa/rfid
    
    나머지 헤더파일 : 아두이노 기본 헤더파일 참조
    
    // 일부분의 소스는 베껴서 썼지만, 거의 모든 소스코드를 이민재(나)가 썼음을 알립니다.
    
    //불법적으로 악용할 목적이 아닌 오직 연구목적임을 밝힙니다.

---
+++ DIMI FINAL.ino
@@ -0,0 +1,180 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+ LiquidCrystal_I2C lcd(0x27,16,2);
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  
+MFRC522::MIFARE_Key key; 
+int button123 = 6;
+// Init array that will store new NUID 
+byte nuidPICC[4];
+byte heart[8] = { B00000, B01010, B11111, B11111, B01110, B00100, B00000, B00000};
+
+void setup() { 
+  lcd.init(); // initialize the lcd 
+  lcd.backlight(); // LCD Backlight on
+  lcd.createChar(0, heart); //heart icon 
+pinMode(button123,INPUT_PULLUP);
+  Serial.begin(9600);
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522 
+ 
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+  Serial.println(F("This code scan the MIFARE Classsic NUID."));
+  Serial.print(F("Using the following key:"));
+  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+}
+ 
+void loop() {
+if(digitalRead(button123)==0){
+  // Look for new cards
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    
+   lcd.setCursor(0, 0);
+  lcd.write(0); 
+  lcd.print("Your tag is not of type MIFATE Classic.");
+    return;
+  }
+
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+    rfid.uid.uidByte[1] != nuidPICC[1] || 
+    rfid.uid.uidByte[2] != nuidPICC[2] || 
+    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+    Serial.println(F("A new card has been detected."));
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+   
+    Serial.println(F("The NUID tag is:"));
+    Serial.print(F("In hex: "));
+    printHex(rfid.uid.uidByte, rfid.uid.size);
+
+   lcd.setCursor(0, 0);
+  lcd.write(0); 
+   lcd.print(rfid.uid.uidByte[0],HEX);
+    lcd.print(" ");
+   lcd.print(rfid.uid.uidByte[1],HEX);
+       lcd.print(" ");
+   lcd.print(rfid.uid.uidByte[2],HEX);
+       lcd.print(" ");
+   lcd.print(rfid.uid.uidByte[3],HEX);
+    lcd.setCursor(0, 1);
+   lcd.print("Complete Copy!");
+    Serial.println();
+    Serial.print(F("In dec: "));
+    printDec(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+    }
+  else Serial.println(F("Card read previously."));
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+}
+
+
+
+
+
+if(digitalRead(button123)==1){
+
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+    delay(50);
+    return;
+  }
+  
+  // Now a card is selected. The UID and SAK is in mfrc522.uid.
+  
+  // Dump UID
+  Serial.print(F("Card UID:"));
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  } 
+  Serial.println();
+
+ byte newUid[4];
+  // Set new UID
+  newUid[0] = (rfid.uid.uidByte[0]);
+newUid[1] = (rfid.uid.uidByte[1]);
+ newUid[2] = (rfid.uid.uidByte[2]); 
+newUid[3] = (rfid.uid.uidByte[3]);
+///////////////////////////////////////////////////////
+
+
+  
+  if ( mfrc522.MIFARE_SetUid(newUid, (byte)4, true) ) {
+    Serial.println(F("Wrote new UID to card."));
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.write(0); 
+    lcd.print("SUCCESS!!");
+  }
+  
+  // Halt PICC and re-select it so DumpToSerial doesn't get confused
+  mfrc522.PICC_HaltA();
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+    return;
+  }
+  
+  // Dump the new memory contents
+  Serial.println(F("New UID and contents:"));
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+ 
+  delay(2000);
+}
+
+}
+
+
+
+
+
+
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+/**
+ * Helper routine to dump a byte array as dec values to Serial.
+ */
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
+}
+
+
+
+
